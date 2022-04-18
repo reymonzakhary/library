21fe881d804a24e98103ac08d8afc1774bd7e6cc
khaledhelmy-chd <khaled.helmy@charisma-desgin.eu>
2022-04-18 16:03:03 +0200

    update home screen

--- mobile/book_store/lib/Data/Model/book.dart
+++ mobile/book_store/lib/Data/Model/book.dart
@@ -3,10 +3,19 @@ class Book {
   final String name;
   final String image;
   final String author;
+  final double rate;
+  final int seller;
+  final int download; //reder
+  final int recent;
 
   Book(
       {required this.id,
       required this.name,
       required this.image,
-      required this.author});
+      required this.author,
+      required this.rate,
+      required this.download,
+      required this.recent,
+      required this.seller
+      });
 }
--- mobile/book_store/lib/Presentation/views/screens/homeScreen.dart
+++ mobile/book_store/lib/Presentation/views/screens/homeScreen.dart
@@ -3,11 +3,12 @@ import 'dart:math';
 import 'package:book_store/Data/utilities/constant.dart';
 import 'package:book_store/Data/utilities/textStyle.dart';
 import 'package:book_store/Presentation/controller/homeController.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeScreen extends GetView<HomeController> {
-  final bookImageHeight = 200.0;
+  final bookImageHeight = 240.0;
   final bookImageWidth = 180.0;
   @override
   Widget build(BuildContext context) {
@@ -19,34 +20,27 @@ class HomeScreen extends GetView<HomeController> {
                     _appBarSliver(),
                     _bodySliver()
                 ],
-              
+
               ),
             ));
   }
   _appBarSliver()=> SliverAppBar(
     flexibleSpace: _pageView(),
     automaticallyImplyLeading: false,
-    toolbarHeight: Get.height*0.4,
+    toolbarHeight: 270,
   );
-  _bodySliver()=> SliverList(delegate: SliverChildBuilderDelegate((context , index){
-    if(index ==0 ){
-      return _topSeller();
-    }
-    if(index ==1 ){
-      return _Recent();
-    }
-        if(index ==2 ){
-      return _Recent();
-    }
-        if(index ==3 ){
-      return _Recent();
-    }
-   if(index ==4 ){
-      return _Recent();
-    }
-  },
-  childCount: 5
-  ));
+  _bodySliver()=> SliverToBoxAdapter(
+   child: Column(
+       children: [
+            _topSeller(),
+            _Recent(),
+            _topSeller(),
+            _Recent(),
+            _topSeller(),
+            _Recent(),
+       ],
+   ),
+  );
 
   _pageView() => Container(
         width: Get.width,
@@ -62,12 +56,33 @@ class HomeScreen extends GetView<HomeController> {
               final rotate = current.clamp(0.0, 1.0);
               final book = controller.books[index];
               final fixRotate = pow(rotate, 0.35);
-              return SizedBox(
+              return Container(
                 width: Get.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
+                 padding: EdgeInsets.only(top: 10 , left: 10),
+                 child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
+                      Column(
+                          children: [
+                           Opacity(
+                      opacity: 1 - rotate,
+                      child:Text("${controller.books[index].name}",
+                       style: TextStyle(color: brownColor ,
+                       fontSize: 20 , fontWeight: FontWeight.w800),
+                        textAlign: TextAlign.start),
+
+
+                    ),
+                          Container(width: 140, child: Text("Description of catigories of to expaln how can you get here \n to make idea we m4 store book m4 hat2dr te8amed 3enek \n afred masln masln any 5asmetk yom.",
+                       style: TextStyle(color: brownColor400 ,
+                       fontSize: 12 , fontWeight: FontWeight.w300),
+                        textAlign: TextAlign.start),)
+
+                          ],
+                      ),
+
+                     SizedBox(width: 40),
                     Stack(
                       children: [
                         Container(
@@ -99,11 +114,9 @@ class HomeScreen extends GetView<HomeController> {
                         ),
                       ],
                     ),
-                    Opacity(
-                      opacity: 1 - rotate,
-                      child: Text(book.name),
-                    ),
-                    Text(book.author),
+
+
+
                   ],
                 ),
               );
@@ -113,11 +126,11 @@ class HomeScreen extends GetView<HomeController> {
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
       Container(
-        padding:EdgeInsets.all(8),
-      
+        padding: const EdgeInsets.all(8),
+
         child:
          Text("TOP SELLER", style: TextStyle(color: blueColor400 , fontSize: 18 , fontWeight: FontWeight.w800),)),
-     
+
       Container(
     decoration: BoxDecoration(
                               color: Colors.grey.shade200,
@@ -128,26 +141,26 @@ class HomeScreen extends GetView<HomeController> {
                                     offset: Offset(5.0, 5.0),
                                     spreadRadius: 10)
                               ]),
-    height:140, 
+    height:180,
     child: ListView.builder(
     scrollDirection: Axis.horizontal,
     itemBuilder: (context , index){
-      return Container(padding: EdgeInsets.all(8), child: Image.asset(controller.books[index].image , width: 100, height: 120,),);
+      return Container(padding: EdgeInsets.all(8), child: _bookBody(index));
     },
     itemCount: controller.books.length,),)
     ],
   );
-  
   _Recent() => Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
       Container(
         padding:EdgeInsets.all(8),
-      
+
         child:
          Text("RECENT", style: TextStyle(color: blueColor400 , fontSize: 18 , fontWeight: FontWeight.w800),)),
-     
+
       Container(
+          width: Get.width,
     decoration: BoxDecoration(
                               color: Colors.grey.shade200,
                               boxShadow: [
@@ -157,13 +170,28 @@ class HomeScreen extends GetView<HomeController> {
                                     offset: Offset(5.0, 5.0),
                                     spreadRadius: 10)
                               ]),
-    height:140, 
-    child: ListView.builder(
-    scrollDirection: Axis.horizontal,
-    itemBuilder: (context , index){
-      return Container(padding: EdgeInsets.all(8), child: Image.asset(controller.books[index].image , width: 100, height: 120,),);
-    },
-    itemCount: controller.books.length,),)
+    height:140,
+    child: CarouselSlider.builder(
+        itemCount: controller.books.length,
+        itemBuilder: (context , index , x){
+            return Container(padding: EdgeInsets.all(8), child: Image.asset(controller.books[index].image , width: 100, height: 120,),);
+        }, options: CarouselOptions(
+            viewportFraction: .5,
+            enlargeCenterPage: true,
+            autoPlay: true,
+            pauseAutoPlayInFiniteScroll: true,
+            )) )
     ],
   );
+  _bookBody(index)=>Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+       Image.asset(controller.books[index].image , width: 100, height: 120,),
+       SizedBox(height: 10),
+       Text("${controller.books[index].name}", style: TextStyle(color: brownColor400 , fontSize: 14 , fontWeight: FontWeight.w600), textAlign: TextAlign.center),
+              Text("by: ${controller.books[index].author}", style: TextStyle(color: brownColor100 , fontSize: 14 , fontWeight: FontWeight.w600), textAlign: TextAlign.center)
+      ],
+  );
+
 }
